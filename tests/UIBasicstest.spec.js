0c3b694d57d5208cc2c0c7734d4988fcdf99e841
masud.rana <masudr2030@gmail.com>
2022-05-18 23:30:15 +0600

    Form text

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,7 +1,18 @@
 const {test, expect} = require('@playwright/test');
 
-test('First Test', async ({browser})=>{
+test('Enter First name', async ({browser})=>{
     const context = await browser.newContext();
     const page = await browser.newPage();
-    await page.goto("https://www.google.com/");
+    await page.goto("https://fluentforms.com/forms/contact-form-demo/");
+    await page.locator('#ff_3_names_first_name_').type("Masud");
+    await page.locator('#ff_3_names_last_name_').type("Rana");
+    await page.locator('#ff_3_email').type("mr22@gmail.com");
+    await page.locator('#ff_3_subject').type("Test Subject");
+    await page.locator('#ff_3_message').type("This is the test messages");
+    await page.locator('text=Submit Form').click();
+    //Thank you for your message. We will get in touch with you shortly
+    const loce = page.locator('.ff-message-success');
+    await expect(loce).toHaveText("Thank you for your message. We will get in touch with you shortly");
+
 });
+
